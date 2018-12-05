af8d8cc2b8dc5d6bde5d4c83a9f5ee9983502552
Aleksejs Fomins <aleksejs.fomins@uzh.ch>
2018-12-05 20:32:42 +0100

    stuff

--- python/aux/signal/filter.py
+++ python/aux/signal/filter.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.interpolate import interp1d
 
 # Compute the approximate convolution with decaying exponential,
 # given time-discretization and decay constant
@@ -39,3 +40,23 @@ def approxLPF(data, TAU, DT):
             rez[i+1] = rez[i] * beta + data[nPoint-1]
 
         return rez[1+shiftCorr:] * alpha
+    
+
+# Smoothen data by averaging points over a window
+def windowAvg1D(data, windowSize):
+    ndata = len(data)
+    gap = windowSize // 2
+    rez = np.zeros(ndata + 1)
+    tmp = np.zeros(ndata + 2*gap)
+    tmp[:gap] = 2*data[0] - data[:gap][::-1]
+    tmp[-gap:] = 2*data[-1] - data[-gap:][::-1]
+    tmp[gap:-gap] = data
+    
+    for i in range(ndata + 1):
+        rez[i] = np.average(tmp[i:i+windowSize])
+        
+    # Problem: resulting array is 1 point longer than source. Need to reinterpolate
+    xold = np.linspace(0, 1, len(data)+1)
+    xnew = np.linspace(0, 1, len(data))
+    f2 = interp1d(xold, rez, kind='cubic')
+    return f2(xnew)
