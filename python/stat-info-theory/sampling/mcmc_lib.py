eed99409da0c32a0a3b70cf79ce0d14c32365c7f
Aleksejs Fomins <aleksejs.fomins@uzh.ch>
2019-04-04 19:49:55 +0200

    sampling

---
+++ python/stat-info-theory/sampling/mcmc_lib.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def sample_metropolis_hastings(f, p, x0, N):
+    x_arr = np.zeros((N, len(x0)))
+    y_arr = np.zeros(N)
+    x_arr[0] = x0
+    y_arr[0] = f(x0)
+    for i in range(1, N):
+        x_arr[i] = x_arr[i-1] + p()
+        y_arr[i] = f(x_arr[i])
+        alpha = y_arr[i] / y_arr[i-1]
+        if (alpha < 1) and (np.random.uniform(0, 1) > alpha):
+            x_arr[i] = x_arr[i-1]
+            y_arr[i] = y_arr[i-1]
+    return x_arr, y_arr
